a94ccd11b8c3349fe239e6f215d892a88a72333b
iD Student <student@idacademy-50.Princeton.EDU>
2015-06-29 09:12:22 -0400

    Initial Commit

---
+++ FLAPPY_FIGHTERSTests/FLAPPY_FIGHTERSTests.swift
@@ -0,0 +1,36 @@
+//
+//  FLAPPY_FIGHTERSTests.swift
+//  FLAPPY_FIGHTERSTests
+//
+//  Created by iD Student on 6/29/15.
+//  Copyright (c) 2015 iD Student. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FLAPPY_FIGHTERSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
